75c2b5e1e41b0443a9b6e0e7ec07e4da50da6fec
Benoist Bouteiller <benoist.bouteiller@testwe.eu>
2023-01-21 18:35:08 +0100

    feat(groups): add groups

---
+++ src/figure/groups.js
@@ -0,0 +1,56 @@
+import {
+  CHASSE_AUX_RONDS_DE_CUIR,
+  CHASSE_AU_WOKISME,
+  CONTROLE_POPULATION,
+  IMMIGRATION_ZER0,
+  LEGALISATION_ARMES,
+  LEGALISATION_CANNABIS,
+  PRIX_DE_L_ESSENCE,
+  SMIC_2000,
+  UNIFORME_OBLIGATOIRE,
+} from '../utils/tags'
+
+export const groups = [
+  {
+    name: 'gilets jaunes',
+    sprite: 'assets/group_base.png',
+    force: 1,
+    cost: 8,
+    minPopularity: 0,
+    accepts: [
+      SMIC_2000,
+      ISF,
+      ENERGIE_100_POURCENT_RENOUVELABLE,
+      CHASSE_AUX_RONDS_DE_CUIR,
+      PRIX_DE_L_ESSENCE,
+    ],
+    rejects: [TOUT_NUCLAIRE],
+    color: '#b51212',
+  },
+  {
+    name: 'patriotes',
+    sprite: 'assets/group_base.png',
+    force: 1,
+    cost: 8,
+    minPopularity: 0,
+    accepts: [
+      IMMIGRATION_ZER0,
+      CHASSE_AU_WOKISME,
+      UNIFORME_OBLIGATOIRE,
+      CONTROLE_POPULATION,
+      LEGALISATION_ARMES,
+    ],
+    rejects: [ISF, SMIC_2000],
+    color: '#b51212',
+  },
+  {
+    name: 'étudiants en sociologie',
+    sprite: 'assets/group_base.png',
+    force: 1,
+    cost: 8,
+    minPopularity: 0,
+    accepts: [SMIC_2000, ISF, LEGALISATION_CANNABIS],
+    rejects: [IMMIGRATION_ZER0],
+    color: '#b51212',
+  },
+]
