064a8282384470a00d0bdde438707fd7a662f1b8
Benoist Bouteiller <benoist.bouteiller@testwe.eu>
2023-01-21 18:16:07 +0100

    feat(start):add animation for title

--- src/stage/startStage.js
+++ src/stage/startStage.js
@@ -1,22 +1,18 @@
-import { button } from "../utils/button"
+import { button } from '../utils/button'
 
-export const startStage = ( {score} ) => {
-  add([
-    sprite("title"),   // sprite() component makes it render as a sprite
-    pos(center()),     // pos() component gives it position, also enables movement
-    origin("center"), // origin() component defines the pivot point (defaults to "topleft")
-  ])
-  add([
-    text(`go a head ${score}`, {
-      width: width(),
-    }),
-    pos(12),
+export const startStage = ({ score }) => {
+  const title = add([
+    sprite('title'), // sprite() component makes it render as a sprite
+    pos(center()), // pos() component gives it position, also enables movement
+    origin('center'), // origin() component defines the pivot point (defaults to "topleft")
   ])
 
-  button("Start", 250, 300, () => {
+  button('Start', 250, 300, () => {
     go('selectLeader', { score })
   })
-  button("credit", 250, 360, () => {
+  button('credit', 250, 360, () => {
     go('credit', { score })
   })
+
+  title.play('idle')
 }
